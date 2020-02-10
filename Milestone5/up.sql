7c35f3186e4b8e02993486caf587d19cd1bf1ee7
Megan Traeger <mtraeger@mail.wou.edu>
2020-02-10 15:32:03 -0800

    Fixed Up script, not down

--- Milestone5/up.sql
+++ Milestone5/up.sql
@@ -3,7 +3,7 @@ CREATE TABLE [dbo].[Sports]
 	[ID] INT NOT NULL IDENTITY(1,1),
 	[SportName] NVARCHAR(100),
 	[Season] NVARCHAR(100),
-	PRIMARY KEY (ID)
+	CONSTRAINT [PK_dbo.Sports] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
 CREATE TABLE [dbo].[Teams]
@@ -11,8 +11,8 @@ CREATE TABLE [dbo].[Teams]
 	[ID] INT NOT NULL IDENTITY(1,1),
 	[TeamName] NVARCHAR(200),
 	[SportID] INT,
-	FOREIGN KEY (SportID) REFERENCES [dbo].[Sports](ID),
-	PRIMARY KEY (ID)
+	CONSTRAINT [FK_dbo.Teams_dbo.Sports_SportID] FOREIGN KEY ([SportID]) REFERENCES [dbo].[Sports] ([ID]) ON DELETE CASCADE,
+	CONSTRAINT [PK_dbo.Teams] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
 
@@ -21,7 +21,7 @@ CREATE TABLE [dbo].[Roles]
 (
 	[ID] INT NOT NULL IDENTITY(1,1),
 	[RoleName] NVARCHAR(100),
-	PRIMARY KEY (ID)
+	CONSTRAINT [PK_dbo.Roles] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
 CREATE TABLE [dbo].[Persons]
@@ -40,9 +40,9 @@ CREATE TABLE [dbo].[Persons]
 	[DateAdded] DATE,
 	[RoleID] INT,
 	[TeamID] INT,
-	FOREIGN KEY (RoleID) REFERENCES [dbo].[Roles](ID),
-	FOREIGN KEY (TeamID) REFERENCES [dbo].[Teams](ID),
-	PRIMARY KEY (ID)
+	CONSTRAINT [FK_dbo.Persons_dbo.Roles_RoleID] FOREIGN KEY ([RoleID]) REFERENCES [dbo].[Roles] ([ID]) ON DELETE CASCADE,
+	CONSTRAINT [FK_dbo.Persons_dbo.Teams_TeamID] FOREIGN KEY ([TeamID]) REFERENCES [dbo].[Teams] ([ID]) ON DELETE CASCADE,
+	CONSTRAINT [PK_dbo.Persons] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
 CREATE TABLE [dbo].[WorkOuts]
@@ -50,29 +50,29 @@ CREATE TABLE [dbo].[WorkOuts]
 	[ID] INT NOT NULL IDENTITY(1,1),
 	[Date] DATE,
 	[PersonID] INT,
-	FOREIGN KEY (PersonID) REFERENCES [dbo].[Persons](ID),
-	PRIMARY KEY (ID)
+	CONSTRAINT [FK_dbo.WorkOuts_dbo.Persons_PersonID] FOREIGN KEY ([PersonID]) REFERENCES [dbo].[Persons] ([ID]) ON DELETE CASCADE,
+	CONSTRAINT [PK_dbo.WorkOuts] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
 CREATE TABLE [dbo].[MuscleList]
 (
 	[ID] INT NOT NULL IDENTITY(1,1),
 	[Name] NVARCHAR(200),
-	PRIMARY KEY (ID)
+	CONSTRAINT [PK_dbo.MuscleList] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
 CREATE TABLE [dbo].[MovementList]
 (
 	[ID] INT NOT NULL IDENTITY(1,1),
 	[Name] NVARCHAR(200),
-	PRIMARY KEY (ID)
+	CONSTRAINT [PK_dbo.MovementList] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
 CREATE TABLE [dbo].[Exercise]
 (
 	[ID] INT NOT NULL IDENTITY(1,1),
 	[URL] NVARCHAR(MAX),
-	PRIMARY KEY (ID)
+	CONSTRAINT [PK_dbo.Exercise] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
 CREATE TABLE [dbo].[ExerciseMovements]
@@ -81,10 +81,10 @@ CREATE TABLE [dbo].[ExerciseMovements]
 	[MovementListID] INT NOT NULL,
 	[MuscleListID] INT NOT NULL,
 	[ExerciseID] INT NOT NULL,
-	FOREIGN KEY (MuscleListID) REFERENCES [dbo].[MuscleList](ID),
-	FOREIGN KEY (MovementListID) REFERENCES [dbo].[MovementList](ID),
-	FOREIGN KEY (ExerciseID) REFERENCES [dbo].[Exercise](ID),
-	PRIMARY KEY (ID)
+	CONSTRAINT [FK_dbo.ExerciseMovements_dbo.MuscleList_MuscleListID] FOREIGN KEY ([MuscleListID]) REFERENCES [dbo].[MuscleList] ([ID]) ON DELETE CASCADE,
+	CONSTRAINT [FK_dbo.ExerciseMovements_dbo.MovementList_MovementListID] FOREIGN KEY ([MovementListID]) REFERENCES [dbo].[MovementList] ([ID]) ON DELETE CASCADE,
+	CONSTRAINT [FK_dbo.ExerciseMovements_dbo.Exercise_ExerciseID] FOREIGN KEY ([ExerciseID]) REFERENCES [dbo].[Exercise] ([ID]) ON DELETE CASCADE,
+	CONSTRAINT [PK_dbo.ExerciseMovements] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
 CREATE TABLE [dbo].[ComplexItems]
@@ -97,18 +97,18 @@ CREATE TABLE [dbo].[ComplexItems]
 	[Time] TIME,
 	[Distance] FLOAT,
 	[ExerciseID] INT,
-	FOREIGN KEY (ExerciseID) REFERENCES [dbo].[Exercise](ID),
-	PRIMARY KEY (ID)
+	CONSTRAINT [FK_dbo.ComplexItems_dbo.Exercise_ExerciseID] FOREIGN KEY ([ExerciseID]) REFERENCES [dbo].[Exercise] ([ID]) ON DELETE CASCADE,
+	CONSTRAINT [PK_dbo.ComplexItems] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
 CREATE TABLE [dbo].[Complex]
 (
 	[ID] INT NOT NULL IDENTITY(1,1),
-	[ComplexItemsID] INT,
-	[WorkoutID] INT,
-	FOREIGN KEY (ComplexItemsID) REFERENCES [dbo].[ComplexItems](ID),
-	FOREIGN KEY (WorkoutID) REFERENCES [dbo].[Workouts](ID),
-	PRIMARY KEY (ID)
+	[ComplexItemID] INT,
+	[WorkOutID] INT,
+	CONSTRAINT [FK_dbo.Complex_dbo.ComplexItems_ComplexItemID] FOREIGN KEY ([ComplexItemID]) REFERENCES [dbo].[ComplexItems] ([ID]) ON DELETE CASCADE,
+	CONSTRAINT [FK_dbo.Complex_dbo.WorkOuts_WorkOutID] FOREIGN KEY ([WorkOutID]) REFERENCES [dbo].[WorkOuts] ([ID]) ON DELETE CASCADE,
+	CONSTRAINT [PK_dbo.Complex] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
 
 CREATE TABLE [dbo].[Records]
@@ -118,7 +118,8 @@ CREATE TABLE [dbo].[Records]
 	[Time] TIME,
 	[PersonID] INT,
 	[ExerciseID] INT,
-	FOREIGN KEY (PersonID) REFERENCES [dbo].[Persons](ID),
+	CONSTRAINT [FK_dbo.Records_dbo.Persons_PersonID] FOREIGN KEY ([PersonID]) REFERENCES [dbo].[Persons] ([ID]) ON DELETE CASCADE,
 	FOREIGN KEY (ExerciseID) REFERENCES [dbo].[Exercise](ID),
-	PRIMARY KEY (ID)
+	CONSTRAINT [FK_dbo.Records_dbo.Exercise_ExerciseID] FOREIGN KEY ([ExerciseID]) REFERENCES [dbo].[Exercise] ([ID]) ON DELETE CASCADE,
+	CONSTRAINT [PK_dbo.Records] PRIMARY KEY CLUSTERED ([ID] ASC)
 );
