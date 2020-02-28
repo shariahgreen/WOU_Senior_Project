f8e5d99b67c63d47a5be92814d0efb5b1f41b981
jmcworth <collinsworth.joe@gmail.com>
2020-02-28 10:56:35 -0800

    Added using DAL statement to admin controller

--- PeakPerformance/Peak Performance/Controllers/AdminController.cs
+++ PeakPerformance/Peak Performance/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Peak_Performance.DAL;
 using Peak_Performance.Models;
 
 namespace Peak_Performance.Controllers
