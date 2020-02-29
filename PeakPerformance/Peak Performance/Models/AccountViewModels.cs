84031e0cfeac8e7ec726aff48de179b4247ab6b4
jmcworth <collinsworth.joe@gmail.com>
2020-02-28 16:22:02 -0800

    Added email domain validation

--- PeakPerformance/Peak Performance/Models/AccountViewModels.cs
+++ PeakPerformance/Peak Performance/Models/AccountViewModels.cs
@@ -70,6 +70,7 @@ namespace Peak_Performance.Models
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+(@wou\.edu)$", ErrorMessage = "Registration is limited to the wou.edu domain.")]
         public string Email { get; set; }
 
         [Required]
