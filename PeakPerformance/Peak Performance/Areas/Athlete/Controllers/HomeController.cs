a8e0d788a468cae243ee75d0ab6a0ddc710fabf4
Sodara Y <sy18@mail.wou.edu>
2020-04-07 20:39:45 -0700

    worked one athlete profile

--- PeakPerformance/Peak Performance/Areas/Athlete/Controllers/HomeController.cs
+++ PeakPerformance/Peak Performance/Areas/Athlete/Controllers/HomeController.cs
@@ -1,19 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using Peak_Performance.DAL;
-using Peak_Performance.Models;
+﻿
 
 namespace Peak_Performance.Areas.Athlete.Controllers {
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using Microsoft.AspNet.Identity;
+    using Peak_Performance.DAL;
+    using Peak_Performance.Models;
+    using Peak_Performance.Models.ViewModels;
 
     [Authorize(Roles = "Athlete")]
     public class HomeController : Controller {
         private PeakPerformanceContext db = new PeakPerformanceContext();
 
         public ActionResult Index() {
-            return View();
+
+            string id = User.Identity.GetUserId();
+            Person temp = db.Persons.FirstOrDefault(p => p.ASPNetIdentityID == id);
+            AthleteProfileViewModel athlete = new AthleteProfileViewModel(temp.ID);
+
+            return View("Index", athlete);
         }
     }
 }
