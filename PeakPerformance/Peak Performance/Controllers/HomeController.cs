d88f18b6ea14f6647e1e1d42c0533617e58f658d
Sodara Y <sy18@mail.wou.edu>
2020-04-11 20:58:12 -0700

    contact form worked

--- PeakPerformance/Peak Performance/Controllers/HomeController.cs
+++ PeakPerformance/Peak Performance/Controllers/HomeController.cs
@@ -8,9 +8,23 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Peak_Performance.DAL;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading;
 
 namespace Peak_Performance.Controllers
 {
+    public class EmailFormModel
+    {
+        [Required, Display(Name = "Your name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Your email"), EmailAddress]
+        public string FromEmail { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
     public class HomeController : Controller
     {
         private readonly PeakPerformanceContext db = new PeakPerformanceContext();
@@ -34,6 +48,47 @@ namespace Peak_Performance.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult SendEmail(string receiver, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("ysodara89@gmail.com", "Tesing Emails");
+                    var receiverEmail = new MailAddress("ysodara89@gmail.com", "Receiver");
+                    var password = "sodaraman";
+                    var sub = "ysodara89@gmail.com";
+                    var body = "ysodara89@gmail.com";
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = subject,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return View();
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return View();
+        }
+
+
 
         //This is stricly for testing the example test so we can see how things work.
         public string Capitolize(string sentence)
---
+++ PeakPerformance/Peak Performance/Scripts/mail.js
@@ -0,0 +1,17 @@
+﻿//<input name="firstName" id="firstName" type="text" class="feedback-input" placeholder="First Name" />
+//    <input name="lastName" id="lastName" type="text" class="feedback-input" placeholder="Last Name" />
+//    <input name="email" id="email" type="text" class="feedback-input" placeholder="Email" />
+//    <input name="subject" id="subject" type="text" class="feedback-input" placeholder="subject" />
+//    <textarea name="text" class="feedback-input" placeholder="Comment"></textarea>
+function SendEmail() {
+    $.ajax({
+        type: 'Post',
+        url: "/Home/SendEmail",
+        success: show,
+        error: errorOnAjax
+    }); 
+}
+function show(data) { console.log('Succeed ajax');}
+function errorOnAjax() {
+    console.log('Error on AJAX return');
+}
