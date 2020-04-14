02159e3a08747145684d6028bdf021edea853feb
jmcworth <collinsworth.joe@gmail.com>
2020-04-14 14:43:43 -0700

    Uncommented workout controller code

--- PeakPerformance/Peak Performance/Areas/Coach/Controllers/WorkoutsController.cs
+++ PeakPerformance/Peak Performance/Areas/Coach/Controllers/WorkoutsController.cs
@@ -178,23 +178,19 @@ namespace Peak_Performance.Areas.Coach
     }
 
 
-//    public class RequireRouteValuesAttribute : ActionMethodSelectorAttribute
-//    {
-//        public RequireRouteValuesAttribute(string[] valueNames)
-//        {
-//            ValueNames = valueNames;
-//        }
+    public class RequireRouteValuesAttribute : ActionMethodSelectorAttribute {
+        public RequireRouteValuesAttribute(string[] valueNames) {
+            ValueNames = valueNames;
+        }
 
-//        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
-//        {
-//            bool contains = false;
-//            foreach (var value in ValueNames)
-//            {
-//                contains = controllerContext.HttpContext.Request[value] != null;
-//                if (!contains) break;
-//            }
-//            return contains;
-//        }
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo) {
+            bool contains = false;
+            foreach (var value in ValueNames) {
+                contains = controllerContext.HttpContext.Request[value] != null;
+                if (!contains) break;
+            }
+            return contains;
+        }
 
         public string[] ValueNames { get; private set; }
     }
